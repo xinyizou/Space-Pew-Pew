24672bc615381d4933a7a73ef95d9366bcf80a48
Jing Yao <jinghaoyao_@hotmail.com>
2018-09-29 14:53:45 -0400

    Add files via upload

---
+++ Assets/Scripts/GreenEnemyScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenEnemyScript : MonoBehaviour {
+
+
+	void Start(){
+		
+	}
+	
+	void Update(){
+        
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
