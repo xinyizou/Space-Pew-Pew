81fc566759765ad685fb9b725f67de4793864de6
Jing Yao <jinghaoyao_@hotmail.com>
2018-09-29 13:57:36 -0400

    Added Files as of 1:56 Sept 29 2018

---
+++ Assets/Scripts/GreenEnemyScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenEnemyScript : MonoBehaviour {
+
+	void Start(){
+		
+	}
+	
+	void Update(){
+		
+	}
+}
